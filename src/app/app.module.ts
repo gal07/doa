8712d4e16eb065edf15f419c6aa18f02986026a7
galih <galihkur29@gmail.com>
2019-07-23 19:54:49 +0700

    add angularfire2 for database

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { NativeAudio } from '@ionic-native/native-audio';
-
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule, AngularFireDatabase } from '@angular/fire/database';
+import { AngularFireAuthModule } from '@angular/fire/auth';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -15,6 +17,15 @@ import { SholatPage } from './../pages/sholat/sholat';
 import { WudhuPage } from './../pages/wudhu/wudhu';
 import { DoaHarianExtendPage } from "../pages/doa-harian-extend/doa-harian-extend";
 import { MediaPlayProvider } from '../providers/media-play/media-play';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyC7Hi9Ft_K0MmOdbP6tsg5YksP4F6nORHE",
+  authDomain: "fir-crud-7f287.firebaseapp.com",
+  databaseURL: "https://fir-crud-7f287.firebaseio.com",
+  projectId: "fir-crud-7f287",
+  storageBucket: "fir-crud-7f287.appspot.com",
+  messagingSenderId: "148921659125"
+};
 
 @NgModule({
   declarations: [
@@ -28,7 +39,10 @@ import { MediaPlayProvider } from '../providers/media-play/media-play';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -45,7 +59,8 @@ import { MediaPlayProvider } from '../providers/media-play/media-play';
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
     NativeAudio,
-    MediaPlayProvider
+    MediaPlayProvider,
+    AngularFireDatabase
   ]
 })
 export class AppModule {}
